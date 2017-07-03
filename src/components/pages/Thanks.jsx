ca1ed72452620eb4561a087c4f06e2e6b5964af8
Tanya Powell <hello@tanyapowell.co.uk>
2017-07-03 22:37:14 +0100

    tidying up of thanks component

--- src/components/pages/Thanks.jsx
+++ src/components/pages/Thanks.jsx
@@ -1,31 +1,21 @@
 import React from 'react';
-import { Button, Col, Image, PageHeader, Row } from 'react-bootstrap';
-import { Link } from 'react-router';
-
-import { speaking } from '../../content/content.js';
 
 export default class Talks extends React.Component {
   render() {
-    const displayTalks = Object.keys(speaking.talks).map((key, index) => {
-      return (
-        <Col xs={12} md={4} sm={6} key={ index } className="talk">
-            <Image src={ speaking.talks[key].img } alt={ speaking.talks[key].title
-            } thumbnail/>
-            <div className="caption">
-              <h5 className="heading looped-heading">{ speaking.talks[key].title }</h5>
-              <p className="justified">{ speaking.talks[key].desc }</p>
-              <a href={ speaking.talks[key].slides }>
-                <i className="fa fa-eye fa-3x center" aria-hidden="true" />
-              </a>
-            </div>
-        </Col>
-      );
-    });
-
     return (
-      <div>
-        <div>
-            <p>Thanks</p>
+      <div id="thanks-background">
+        <div className="thanks-content">
+          <h1>Thanks</h1>
+          <h1>Thanks</h1>
+          <h1>Thanks</h1>
+          <h1>Thanks</h1>
+          <h1>Thanks</h1>
+          <h1>Thanks</h1>
+          <h1>Thanks</h1>
+          <h1>Thanks</h1>
+          <h1>Thanks</h1>
+          <h1>Thanks</h1>
+          <h1>Thanks</h1>
         </div>
       </div>
     );
